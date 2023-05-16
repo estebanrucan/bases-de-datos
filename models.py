666f894dd87fe7066a5d5f81e42374de8ff66fb3
Esteban Rucán Carrasco <69557221+estebanrucan@users.noreply.github.com>
2023-05-15 20:25:38 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
 class Persona:
     def __init__(self, edad, altura, peso, sexo):
-        self.edad = edad
-        self.altura = altura
-        self.peso = peso
-        self.sexo = sexo
+        self.edad      = edad
+        self.altura    = altura
+        self.peso      = peso
+        self.sexo      = sexo
+        self.productos = []
+        
+    def comprar_producto(self, producto):
+        self.productos.append(producto)
+        print(f"Haz comprado {producto.nombre} por ${producto.precio}.")
+        
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
